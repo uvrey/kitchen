ff2b58e8b48a7641cda7079a77e23a517cb1563e
uvrey <77244149+uvrey@users.noreply.github.com>
2022-08-16 12:59:26 +0200

    scaled grid items

--- kitchen/animation.py
+++ kitchen/animation.py
@@ -984,7 +984,7 @@ class ManimParseTable(m.Scene):
 
         # set up new value with colour
         t_new = m.MathTex(_to_math_tex(new_val))
-        t_new.scale_to_fit_width(GRID_ITEM_SCALE)
+        t_new.scale_to_fit_width(GRID_ITEM_SCALE).scale(0.7)
         t_new.move_to(t_old)
         t_new.fade_to(config.opp_col(), alpha=0.2)
 
